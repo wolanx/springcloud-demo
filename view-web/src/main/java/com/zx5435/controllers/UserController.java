5af6e29b92c1d6232aaf0375657273118a99403e
zx5435 <825407762@qq.com>
2019-06-10 19:07:24 +0800

    try redirect

--- view-web/src/main/java/com/zx5435/controllers/UserController.java
+++ view-web/src/main/java/com/zx5435/controllers/UserController.java
@@ -1,11 +1,17 @@
 package com.zx5435.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class UserController {
+
+    @Autowired
+    HttpServletRequest request;
 
     @RequestMapping("/user")
     public String index(Model m) {
@@ -16,6 +22,17 @@ public class UserController {
 
     @RequestMapping("/user/login")
     public String login(Model m) {
+        String method = request.getMethod();
+        System.out.println("method = " + method);
+
+        if ("POST".equals(method)) {
+            String username = request.getParameter("username");
+            System.out.println("username = " + username);
+            if ("qwe".equals(username)) {
+                return "redirect:/user?" + username;
+            }
+        }
+
         return "user/login";
     }
 
