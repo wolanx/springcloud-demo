7d5eb1e13670588cef9f7888a1d6db655b594149
zx5435 <825407762@qq.com>
2019-06-19 01:08:16 +0800

    try again

--- view-web/src/main/java/com/zx5435/pcmoto/web/controllers/UserController.java
+++ view-web/src/main/java/com/zx5435/pcmoto/web/controllers/UserController.java
@@ -3,7 +3,7 @@ package com.zx5435.pcmoto.web.controllers;
 import com.zx5435.pcmoto.web.config.LogUtil;
 import com.zx5435.pcmoto.web.config.LogConfig;
 import com.zx5435.pcmoto.common.base.User;
-import groovy.util.logging.Slf4j;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -34,7 +34,7 @@ public class UserController {
         LogConfig.log.info("method = " + method);
         LogUtil.info("user = " + user);
 
-//        log.info("qwe");
+        log.info("qwe");
 
         if ("POST".equals(method)) {
             String username = request.getParameter("username");
