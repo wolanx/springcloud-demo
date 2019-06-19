53c29b2a18aa05a1dfce8b12dc262352cea12a6f
zx5435 <825407762@qq.com>
2019-06-19 16:26:34 +0800

    config log

--- view-web/src/main/java/com/zx5435/pcmoto/web/config/LogConfig.java
+++ view-web/src/main/java/com/zx5435/pcmoto/web/config/LogConfig.java
@@ -1,6 +1,10 @@
 package com.zx5435.pcmoto.web.config;
 
-import org.slf4j.Logger;
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.encoder.PatternLayoutEncoder;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.ConsoleAppender;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,11 +12,31 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class LogConfig {
 
-    final public static Logger log = LoggerFactory.getLogger(LogConfig.class);
+    final public static Logger log = (Logger) LoggerFactory.getLogger(LogConfig.class);
 
     @Bean
     public void logMethod() {
         log.info("==========print log==========");
+        log.info("(LogConfig.java:15)");
+
+//        Logger root = (Logger) LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
+//        LoggerContext ctx = root.getLoggerContext();
+//        ctx.reset();
+//
+//        PatternLayoutEncoder encode = new PatternLayoutEncoder();
+//        encode.setContext(ctx);
+//        encode.setPattern("%-5level %replace((%file:%line]){']', ')'} [%thread]: %message%n");
+//        encode.start();
+//
+//        ConsoleAppender<ILoggingEvent> appender = new ConsoleAppender<>();
+//        appender.setContext(ctx);
+//        appender.setEncoder(encode);
+//        appender.start();
+//
+//        root.addAppender(appender);
+//
+//        root.debug("Message 1");
+//        root.warn("Message 2");
     }
 
 }
