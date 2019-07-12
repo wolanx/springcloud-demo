9164e572c27e056bc0a0e28fe5f98bdb14ea5955
赵煜杰 <825407762@qq.com>
2019-07-12 22:59:45 +0800

    test

--- view-admin/src/main/java/com/zx5435/pcmoto/admin/controllers/NewsController.java
+++ view-admin/src/main/java/com/zx5435/pcmoto/admin/controllers/NewsController.java
@@ -1,15 +1,36 @@
 package com.zx5435.pcmoto.admin.controllers;
 
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 @Controller
+@Slf4j
 public class NewsController {
+
+    @Autowired
+    Aasdasd aasdasd;
+
+    @Value("${info.author}")
+    public String name;
 
     @RequestMapping("/news")
     public String index(Model m) {
         m.addAttribute("arr");
+
+        log.info(name);
+
+        System.out.println("aasdasd = " + aasdasd.author);
 
         return "news/index";
     }
@@ -22,4 +43,11 @@ public class NewsController {
         return "news/index";
     }
 
+}
+
+@Component
+@ConfigurationProperties(prefix = "info")
+@Data
+class Aasdasd {
+    public String author;
 }
--- view-admin/src/main/java/com/zx5435/pcmoto/demo/Demo1.java
+++ view-admin/src/main/java/com/zx5435/pcmoto/demo/Demo1.java
@@ -1,46 +1,66 @@
 package com.zx5435.pcmoto.demo;
 
-import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.List;
-import java.util.concurrent.locks.ReentrantLock;
+import org.springframework.beans.factory.annotation.Value;
 
 public class Demo1 {
 
+    @Value("${info.author}")
+    public String name;
+
+    public static void quickSort(int[] arr, int low, int high) {
+        int i, j, temp, t;
+        if (low > high) {
+            return;
+        }
+        i = low;
+        j = high;
+        //temp就是基准位
+        temp = arr[low];
+
+        while (i < j) {
+            //先看右边，依次往左递减
+            while (temp <= arr[j] && i < j) {
+                j--;
+            }
+            //再看左边，依次往右递增
+            while (temp >= arr[i] && i < j) {
+                i++;
+            }
+            //如果满足条件则交换
+            if (i < j) {
+                t = arr[j];
+                arr[j] = arr[i];
+                arr[i] = t;
+            }
+
+        }
+        //最后将基准为与i和j相等位置的数字交换
+        arr[low] = arr[i];
+        arr[i] = temp;
+        show(arr);
+        //递归调用左半数组
+        quickSort(arr, low, j - 1);
+        //递归调用右半数组
+        quickSort(arr, j + 1, high);
+    }
+
+    public static void show(int[] a) {
+        for (int i : a) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
     public static void main(String[] args) {
-
-        List<Integer> a = new ArrayList<>();
-        a.add(222);
-        a.add(333);
-        a.add(123);
-
-        Iterator<Integer> iterator = a.iterator();
-
-        while (iterator.hasNext()) {
-            Integer next = iterator.next();
-            System.out.println(next);
+        int[] arr = {10, 7, 2, 4, 7, 62, 3, 4, 2, 1, 8, 9, 19};
+        quickSort(arr, 0, arr.length - 1);
+        for (int i1 : arr) {
+            System.out.println(i1);
         }
 
-        List<Integer> integers = Collections.unmodifiableList(a);
+        Demo1 a = new Demo1();
 
-        ReentrantLock r = new ReentrantLock();
-        r.lock();
-
-        try {
-            System.out.println("qwe = 1");
-        } finally {
-            r.unlock();
-        }
-
+        System.out.println(a.name);
     }
 
-}
-
-abstract class Cat {
-    public static void sayHi() {
-        System.out.println("hi~");
-    }
 }
